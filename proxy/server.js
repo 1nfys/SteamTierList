3084a4cffe29377f3ae08fc26f059fb12c4c9d3e
1nfys <jopaosla@mail.ru>
2026-05-25 11:43:34 +0300

    clear code of proxy

--- proxy/server.js
+++ proxy/server.js
@@ -3,28 +3,23 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = process.env.PORT || 20124;
-const PROXY_SECRET = process.env.PROXY_SECRET || 'ВАШ_СЕКРЕТНЫЙ_ПАРОЛЬ_ЗДЕСЬ';
+const PROXY_SECRET = process.env.PROXY_SECRET;
 const WORKER_URL = 'https://stl.curly2089.workers.dev';
 
-// Healthcheck для Pterodactyl
 app.get('/ping', (req, res) => res.send('pong'));
 
-// Настраиваем проксирование
 app.use('/', createProxyMiddleware({
     target: WORKER_URL,
     changeOrigin: true,
     ws: true,
     on: {
         proxyReq: (proxyReq, req, res) => {
-            // Пытаемся получить IP от Ngrok (x-forwarded-for) или берем прямое подключение
             const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-            
+
             if (clientIp) {
-                // Передаем реальный IP в Воркер
                 proxyReq.setHeader('x-forwarded-for', clientIp);
             }
-            
-            // Добавляем наш секрет, чтобы Воркер знал, что нам можно доверять
+
             proxyReq.setHeader('x-proxy-secret', PROXY_SECRET);
         }
     }
